e1bf0c334d36bdc139696ff5ef224a0ed385da87
Gabriel Alejandro Cantú <gabrielalejandrocantu@hotmail.com>
2026-01-16 12:19:29 -0300

    Correcion error en el nav

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ const register = document.querySelector("#register");
 
 btnMenu.addEventListener("click", () => {
   if (
-    menu.classList.contains("nav-close") ||
-    register.classList.contains(".container-register-close")
+    menu.classList.contains("nav-open") ||
+    register.classList.contains(".container-register")
   ) {
-    menu.classList.add("nav-open");
-    menu.classList.remove("nav-close");
-
-    register.classList.add("container-register");
-    register.classList.remove("container-register-close");
-  } else {
     menu.classList.remove("nav-open");
     menu.classList.add("nav-close");
 
     register.classList.remove("container-register");
     register.classList.add("container-register-close");
+  } else {
+    menu.classList.add("nav-open");
+    menu.classList.remove("nav-close");
+
+    register.classList.add("container-register");
+    register.classList.remove("container-register-close");
   }
 });
